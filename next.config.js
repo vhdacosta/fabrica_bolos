0b7dd3e41f5079559df33bc0d705192434076266
Victor Hugo da Costa <victor.cfernandes91@gmail.com>
2025-12-11 13:44:01 -0300

    fix: corrige configuração GitHub Pages

--- next.config.js
+++ next.config.js
@@ -4,8 +4,8 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/fabrica_bolos' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/fabrica_bolos/' : '',
+  basePath: '/fabrica_bolos',
+  trailingSlash: true,
 }
 
 module.exports = nextConfig
